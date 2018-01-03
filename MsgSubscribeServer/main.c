133eee98c5a79d2da10b76246fd1d0bb53bd8ffb
whshiyun <whshiyun@users.noreply.github.com>
2018-01-03 15:52:43 +0800

    change to github
    change to github

---
+++ MsgSubscribeServer/main.c
@@ -0,0 +1,16 @@
+/***********************************************
+ *  /MsgSubscribeServer/main.c
+ *
+ *  Created on: 2018Äê1ÔÂ3ÈÕ
+ *  Author: gift
+ *
+ ***********************************************/
+#include <stdio.h>
+
+int main() {
+
+	printf("power server run!!\r\n");
+
+	return 0;
+}
+
